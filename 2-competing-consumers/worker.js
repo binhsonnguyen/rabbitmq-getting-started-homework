3fe3e4b2d8e0e2e9448dbdcae68044b1f1614ddb
son <son@codegym.vn>
2022-10-21 23:52:49 +0700

    ack after consumer

--- 2-competing-consumers/worker.js
+++ 2-competing-consumers/worker.js
@@ -1,9 +1,39 @@
-const SECONDS = 1000
+#!/usr/bin/env node
+require('dotenv').config()
 
-module.exports = function (msg) {
-  const difficulty = msg.content.toString().split('.').length
-  console.log('[x] received %s', msg.content.toString())
-  setTimeout(function () {
-    console.log('[x] Done!')
-  }, difficulty * SECONDS)
-}
+const amqp = require('amqplib/callback_api');
+const ConnectionStringBuilder = require('../connection-string-builder')
+
+const SECONDS = 1000
+const connectionString = new ConnectionStringBuilder().fromEnv().build()
+
+amqp.connect(connectionString, function (err1, connection) {
+  if (!!err1) {
+    throw err1
+  }
+  connection.createChannel(function (err2, channel) {
+    if (!!err2) {
+      throw err2
+    }
+    const queue = {
+      name: 'task_queue',
+      options: {
+        durable: false
+      }
+    }
+    channel.assertQueue(queue.name, queue.options)
+
+    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+    const workerOptions = {
+      noAck: false
+    }
+    channel.consume(queue.name, function (msg) {
+      const difficulty = msg.content.toString().split('.').length
+      console.log('[x] received %s', msg.content.toString())
+      setTimeout(function () {
+        console.log('[x] Done!')
+        channel.ack(msg)
+      }, difficulty * SECONDS)
+    }, workerOptions)
+  })
+})
